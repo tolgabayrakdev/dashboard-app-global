02374e02bb94c3a401f7d8c2500b0584cd2cbe6f
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-07-01 09:39:56 +0300

    auth service with return

--- server/src/controller/auth-controller.ts
+++ server/src/controller/auth-controller.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from "express";
 import { AuthService } from "../service/auth-service";
-import { error } from "console";
-import { User } from "../schema/auth/User";
 
 interface LoginRequestDTO {
     email: string;
@@ -17,10 +15,11 @@ export class AuthController {
     public login = async (req: Request, res: Response): Promise<void> => {
         try {
             const { email, password }: LoginRequestDTO = req.body;
-            await this.authService.login(email, password);
+            const result = await this.authService.login(email, password);
+            console.log(result);
             res.status(200).json({ success: true, message: "Login is succesful" });
-        } catch (e) {
-            res.status(400).json({ success: false, message: error });
+        } catch (error: any) {
+            res.status(400).json({ success: false, message: error.message });
         }
     }
 }
