03039426ff4af33aeff967582265e92dd3ec3808
Tolga Bayrak <tolgabayrak@Tolga-MacBook-Air.local>
2023-07-11 17:47:27 +0300

    server db connection

--- server/src/db.ts
+++ server/src/db.ts
@@ -8,8 +8,6 @@ const client = new pg.Client({
   port: 5432,
 });
 
-// client nesnesini kullanarak işlemler gerçekleştirme
-
 client.connect(function (err) {
   if (err) throw err;
   console.log('⚡️[Database]: PostgreSQL is connected');
